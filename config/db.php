d0dfdafcd9ae7ee276872bd574b4655b19e3cebe
nilsenj <nikoleivan@gmail.com>
2016-08-15 18:36:30 +0300

    initial commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost:33060;dbname=bless',
+    'username' => 'homestead',
+    'password' => 'secret',
+    'charset' => 'utf8',
+];
